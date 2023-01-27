402739620ac483f8f688f574e210c84b3db2f199
MaximShtaba <111359641+MaximShtaba@users.noreply.github.com>
2023-01-27 01:17:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function towelSort (matrix) {
 	let reduseValue;
-	if ((matrix.length) == 0) {return matrix = [ ]} else {
+	if (typeof matrix !== "object") {return matrix = [ ]} else {
 	 reduseValue = matrix.reduce(function(previousValue, item, index, array){
 		 item.sort((a,b)=>(index%2===0)? a-b:b-a).map(index=>previousValue.push(index));
 		return previousValue; }, [ ] ); 
